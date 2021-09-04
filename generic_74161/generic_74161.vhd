b77f2d711311e8f7f55f8ad66bbdb676548c2ccb
ti-nspire <29220357+ti-nspire@users.noreply.github.com>
2021-09-04 15:18:46 +0900

    Update generic_74161.vhd

--- generic_74161/generic_74161.vhd
+++ generic_74161/generic_74161.vhd
@@ -25,7 +25,7 @@ architecture rtl of generic_74161 is
 begin
 
 	q   <= std_logic_vector(to_unsigned(count, NUM_BITS));
-	rco <= '1' when t = '1' and count >= 2**NUM_BITS - 1 else '0';
+	rco <= '1' when t = '1' and count >= COUNT_MAX else '0';
 
 	process(all)
 	begin
