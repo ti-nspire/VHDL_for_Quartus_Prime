4ec80a9543cd2af600c4b82449f959914f1a2438
ti-nspire <29220357+ti-nspire@users.noreply.github.com>
2021-11-03 18:39:48 +0900

    Update generic_counter_updown.vhd

--- generic_counter_updown_ver2/generic_counter_updown.vhd
+++ generic_counter_updown_ver2/generic_counter_updown.vhd
@@ -21,18 +21,24 @@ begin
 
 	process(clk, aclr_n)
 		variable count : natural range BOTTOM_VAL to TOP_VAL := BOTTOM_VAL;
-		variable diff  : integer range -1 to 1 := 1;
+		variable diff  : integer range         -1 to 1       := 1;
+		variable is_climbing_temp : std_logic := '1';
 	begin
 		if aclr_n = '0' then
 			count := BOTTOM_VAL;
-			is_climbing <= '1';
+			is_climbing_temp := '1';
 		elsif rising_edge(clk) then
-			if    count <= BOTTOM_VAL then diff :=  1; is_climbing <= '1';
-			elsif count >= TOP_VAL    then diff := -1; is_climbing <= '0';
+			if    count <= BOTTOM_VAL then diff :=  1;
+			elsif count >= TOP_VAL    then diff := -1;
 			end if;
 			count := count + diff;
+		elsif falling_edge(clk) then
+			if    count <= BOTTOM_VAL then is_climbing_temp := '1';
+			elsif count >= TOP_VAL    then is_climbing_temp := '0';
+			end if;
 		end if;
-		q <= std_logic_vector(to_unsigned(count, NUM_BITS));
+		q           <= std_logic_vector(to_unsigned(count, NUM_BITS));
+		is_climbing <= is_climbing_temp;
 	end process;
 
 end architecture;
