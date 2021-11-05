172059dfeaf7ec7a24506767180fa78e37f305ff
ti-nspire <29220357+ti-nspire@users.noreply.github.com>
2021-11-05 13:23:13 +0900

    Update generic_counter_updown.vhd

--- generic_counter_updown_ver2/generic_counter_updown.vhd
+++ generic_counter_updown_ver2/generic_counter_updown.vhd
@@ -4,15 +4,14 @@ use ieee.numeric_std.all;
 
 entity generic_counter_updown is
 	generic(
-		TOP_VAL    : natural := 10;
-		BOTTOM_VAL : natural := 5;
-		NUM_BITS   : natural := 4 -- ceil(log2(TOP_VAL + 1))
+		TOP_uns    : unsigned := d"10";
+		BOTTOM_nat : natural  := 5
 	);
 	port(
 		aclr_n      : in  std_logic;
 		clk         : in  std_logic;
 		is_climbing : out std_logic;
-		q           : out std_logic_vector(NUM_BITS-1 downto 0)
+		count       : out std_logic_vector(TOP_uns'reverse_range)
 	);
 end entity;
 
@@ -20,24 +19,25 @@ architecture rtl of generic_counter_updown is
 begin
 
 	process(clk, aclr_n)
-		variable count : natural range BOTTOM_VAL to TOP_VAL := BOTTOM_VAL;
-		variable diff  : integer range         -1 to 1       := 1;
+		constant TOP_nat          : natural := to_integer(TOP_uns);
+		variable count_temp       : natural range BOTTOM_nat to TOP_nat := BOTTOM_nat;
+		variable diff             : integer range -1 to 1 := 1;
 		variable is_climbing_temp : std_logic := '1';
 	begin
 		if aclr_n = '0' then
-			count := BOTTOM_VAL;
+			count_temp := BOTTOM_nat;
 			is_climbing_temp := '1';
 		elsif rising_edge(clk) then
-			if    count <= BOTTOM_VAL then diff :=  1;
-			elsif count >= TOP_VAL    then diff := -1;
+			if    count_temp <= BOTTOM_nat then diff :=  1;
+			elsif count_temp >= TOP_nat    then diff := -1;
 			end if;
-			count := count + diff;
+			count_temp := count_temp + diff;
 		elsif falling_edge(clk) then
-			if    count <= BOTTOM_VAL then is_climbing_temp := '1';
-			elsif count >= TOP_VAL    then is_climbing_temp := '0';
+			if    count_temp <= BOTTOM_nat then is_climbing_temp := '1';
+			elsif count_temp >= TOP_nat    then is_climbing_temp := '0';
 			end if;
 		end if;
-		q           <= std_logic_vector(to_unsigned(count, q'length));
+		count       <= std_logic_vector(to_unsigned(count_temp, count'length));
 		is_climbing <= is_climbing_temp;
 	end process;
 
