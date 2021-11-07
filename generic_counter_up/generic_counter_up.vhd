cb9b201c9073552543eab60b80c8f9f80caa18a1
ti-nspire <29220357+ti-nspire@users.noreply.github.com>
2021-11-08 06:11:20 +0900

    Update generic_counter_up.vhd

--- generic_counter_up/generic_counter_up.vhd
+++ generic_counter_up/generic_counter_up.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity generic_counter_up is
 	generic(
 		TOP_uns    : unsigned := d"20";
-		BOTTOM_nat : natural  := 5
+		BOTTOM_uns : unsigned := d"5"
 	);
 	port(
 		clk    : in  std_logic;
@@ -18,19 +18,19 @@ architecture rtl of generic_counter_up is
 begin
 
 	process(aclr_n, clk)
-		constant TOP_nat    : natural := to_integer(TOP_uns);
-		variable count_temp : natural range BOTTOM_nat to TOP_nat := BOTTOM_nat;
+		constant BOTTOM     : unsigned(count'reverse_range) := resize(BOTTOM_uns, count'length);
+		variable count_temp : unsigned(count'reverse_range) := BOTTOM;
 	begin
 		if aclr_n = '0' then
-			count_temp := BOTTOM_nat;
+			count_temp := BOTTOM;
 		elsif rising_edge(clk) then
-			if count_temp >= TOP_nat then
-				count_temp := BOTTOM_nat;
+			if count_temp  >= TOP_uns then
+				count_temp := BOTTOM;
 			else
 				count_temp := count_temp + 1;
 			end if;
 		end if;
-		count <= std_logic_vector(to_unsigned(count_temp, count'length));
+		count <= std_logic_vector(count_temp);
 	end process;
 
 end architecture;
